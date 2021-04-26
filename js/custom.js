fd8855690611bb9f0c67b7e7a4efa2188678768c
Oscar Quesada <oscaardanielqc@outlook.es>
2021-04-25 19:49:09 -0600

    Commit 22/04/2021
    Implementación de funcionalidad

--- js/custom.js
+++ js/custom.js
@@ -7,19 +7,39 @@ function closeNav() {
 }
 
 (function ($) {
-    // Instantiate MixItUp:
-    $('#Container').mixItUp();
+
+    // Menu filer
+    $("#menu-flters li a").click(function () {
+        $("#menu-flters li a").removeClass('active');
+        $(this).addClass('active');
+
+        var selectedFilter = $(this).data("filter");
+        //  $("#menu-wrapper").fadeTo(100, 0);
+
+        $(".menu-restaurant").fadeOut();
+
+        setTimeout(function () {
+            $(selectedFilter).slideDown();
+            //$("#menu-wrapper").fadeTo(300, 1);
+        }, 300);
+    });
 
     // Add smooth scrolling to all links in navbar + footer link
-    $(".sidenav a").on('click', function(event) {
-        event.preventDefault();
+    $(".sidenav a").on('click', function (event) {
         var hash = this.hash;
+        if (hash) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+                window.location.hash = hash;
+            });
+        }
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function(){
-            window.location.hash = hash;
-        });
     });
-    
+
+    $(".sidenav a").on('click', function () {
+        closeNav();
+    });
+
 })(jQuery);
